de89bff05b7cba3f36c1a844f0878aa41b855c92
Tarachandani <ayush.tarachandani@gmail.com>
2024-02-25 06:48:57 -0600

    final push

--- front/src/Login.js
+++ front/src/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Login.css'; // Import the CSS file
+import './login_style.css'; // Import the CSS file
 
 const Login = () => {
   const handleGoogleLogin = () => {
@@ -7,8 +7,11 @@ const Login = () => {
   };
 
   return (
-    <div className="container"> {/* Add a container class */}
-      <button className="button" onClick={handleGoogleLogin}>Sign in with Google</button> {/* Add a button class */}
+    <div style={{ backgroundColor: '#4CAF50', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ textAlign: 'center', color: '#fff' }}>
+        <h1>NutriHub</h1>
+        <button onClick = {handleGoogleLogin} style={{ padding: '10px 20px', fontSize: '16px', backgroundColor: '#fff', borderRadius: '5px', cursor: 'pointer', border: 'none' }}>Login</button>
+      </div>
     </div>
   );
 };
