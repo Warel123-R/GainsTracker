cd8a7cfdeb1bb7564b48a0aff912d6d39207a2f7
franklinzhang12 <108485185+franklinzhang12@users.noreply.github.com>
2024-02-24 22:06:17 -0600

    Post nutrition

--- CV/barcode_to_nutrients.py
+++ CV/barcode_to_nutrients.py
@@ -1,6 +1,11 @@
 import cv2
 
+import json
 import openfoodfacts
+
+import requests
+
+url = "http://localhost:5004/nutrients"
 # https://github.com/openfoodfacts/openfoodfacts-python
 
 api = openfoodfacts.API()
@@ -15,23 +20,48 @@ cap = cv2.VideoCapture(0)
 capturing = True  # Flag to indicate if capturing is enabled
 cv2.namedWindow('frame')
 
-while True:
-    ret, frame = cap.read()
+def readFrame():
+    while True:
+        ret, frame = cap.read()
 
-    if ret:
-        ret_bc, decoded_info, _, points = bd.detectAndDecodeWithType(frame)
-        if ret_bc:
-            frame = cv2.polylines(frame, points.astype(int), True, (0, 255, 0), 3)
-            for s, p in zip(decoded_info, points):
-                if s:
-                    code = s
-                    print(api.product.get(code, fields=["code", "product_name"]))
-                    frame = cv2.putText(frame, s, p[1].astype(int),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
-        cv2.imshow(window_name, frame)
+        if ret:
+            ret_bc, decoded_info, _, points = bd.detectAndDecodeWithType(frame)
+            if ret_bc:
+                frame = cv2.polylines(frame, points.astype(int), True, (0, 255, 0), 3)
+                for s, p in zip(decoded_info, points):
+                    if s:
+                        code = s
+                        frame = cv2.putText(frame, s, p[1].astype(int),
+                                            cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
+                        return s
+            cv2.imshow(window_name, frame)
 
-    if cv2.waitKey(delay) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(delay) & 0xFF == ord('q'):
+            break
 
+def fill_null(dic, key):
+    if key not in dic.keys():
+        dic[key] = None
+
+code = readFrame()
 cap.release()
 cv2.destroyAllWindows()
+
+product = api.product.get(code, fields=["product_name", "quantity", "nutriments", "nutrition_data_per", "nutrition_data_prepared_per"])
+product = product["product"]
+
+fields = ["quantity", "nutrition_data_per", "nutrition_data_prepared_per"]
+for field in fields:
+    fill_null(product, field)
+
+nutrients = ["carbohydrates", "proteins", "sodium", "energy-kcal", "saturated-fat"]
+for nutrient in nutrients:
+    fill_null(product["nutriments"], nutrient)
+
+product["nutriments"] = {nutrient: product["nutriments"][nutrient] for nutrient in nutrients}
+
+print(json.dumps(product, sort_keys=True, indent=4))
+
+x = requests.post(url, json = product)
+print(x.text)
+# weight = product["packaging_text"]
--- CV/label_api_approach.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-api_url = 'https://api.ocr.space/parse/image'
-image_file_descriptor = open('IMG_1205.jpeg', 'rb')
-files = {'image': image_file_descriptor}
-r = requests.post(api_url, files=files)
-print(r.json())
